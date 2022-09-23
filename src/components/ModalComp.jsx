6a21dbd022944e6384b9df5f75419b28eaec7049
lowc0ff <lowc0ff@protonmail.com>
2022-09-23 10:04:12 -0300

    valores default

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -13,8 +13,18 @@ import {
     Box
 
 } from "@chakra-ui/react"
-const ModalComp = () => {
-    return <></>
+import { useState } from "react"
+
+
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+    const [name, setName] = useState(dataEdit.name || "")
+    const [email, setEmail] = useState(dataEdit.email || "")
+    return (
+        <>
+            <ModaL isOpen={isOpen} onClose={onClose}>
+            </ModaL>
+        </>
+    )
 }
 
 export default ModalComp
