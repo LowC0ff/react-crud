6ca1ee7cb8c957aa6eaadccf1646bf8eefe9ec6c
lowc0ff <lowc0ff@protonmail.com>
2022-09-23 09:59:14 -0300

    components modal

---
+++ src/components/ModalComp.jsx
@@ -0,0 +1,20 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Box
+
+} from "@chakra-ui/react"
+const ModalComp = () => {
+    return <></>
+}
+
+export default ModalComp
