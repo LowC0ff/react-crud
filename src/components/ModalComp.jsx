296c3c9fe3e99357eebac966550e437083aaf12c
lowc0ff <lowc0ff@protonmail.com>
2022-09-27 09:25:15 -0300

    finish modal comp

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -19,17 +19,85 @@ import { useState } from "react"
 const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
     const [name, setName] = useState(dataEdit.name || "")
     const [email, setEmail] = useState(dataEdit.email || "")
+
+    const handleSave = () => {
+        if (!name || !email) return
+
+        if (emailAlreadyExists()){
+            return Alert("Email ja cadastrado!")
+        }
+
+        if (Object.keys(dataEdit).length){
+            data[dataEdit.index] = { name, email }
+        }
+
+        const newDataArray = !Object.keys(dataEdit).length
+            ? [...(data ? data : []), {name, email }]
+            : [...(data ? data : [])]
+
+        localStorage.setItem("cad_cliente", JSON.stringfy(newDataArray))
+
+        setData(newDataArray)
+
+        onClose()
+    }
+
+    const emailAlreadyExists = () => {
+        if (dataEdit.email !== email && data?.length){
+            return data.find((item) => item.email === email)
+        }
+    }
+
     return (
         <>
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal 
+                isOpen={isOpen} 
+                onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Cadastro de Clientes</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <FormControl display="flex" flexDir="colum" gap={4}>
+                        <FormControl 
+                            display="flex" 
+                            flexDir="colum" 
+                            gap={4}
+                        >
+
+                            <Box>
+                                <FormLabel>Nome</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </Box>
+                            <Box>
+                            <FormLabel>Email</FormLabel>
+                                <Input
+                                    type="email"
+                                    value={name}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Box>
                         </FormControl>
                     </ModalBody>
+
+                    <ModalFooter justifyContent="start">
+                        <Button
+                            colorScheme="green" 
+                            mr={3}
+                            onClick={handleSave}
+                        >
+                            SALVAR
+                        </Button>
+                        <Button
+                            colorScheme="red" 
+                            onClick={onClose}
+                        >
+                            CANCELAR
+                        </Button>
+                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
